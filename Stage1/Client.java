9a2a936b653d94f443eef044537381b419140690
avagardiner <88016022+avagardiner@users.noreply.github.com>
2023-04-02 09:13:06 +1000

    Modify your client to do the handshaking (up to Step 6 in Section 6.1. in the user guide).

--- Stage1/Client.java
+++ Stage1/Client.java
@@ -3,7 +3,6 @@ import java.net.InetAddress;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
@@ -16,11 +15,40 @@ public class Client {
             System.out.println("Target IP: " + s.getInetAddress() + " Target Port: " + s.getPort());
             System.out.println("Local IP: " + s.getLocalAddress() + " Local Port: " + s.getLocalPort());
 
+            // Step 1: Send HELO to server
             out.writeUTF("HELO\n");
             out.flush();
             System.out.println("SENT: HELO");
 
+            // Step 2: Wait for OK response from server
             String str = in.readLine();
+            if (str.startsWith("OK")) {
+                System.out.println("RCVD: " + str);
+            } else {
+                throw new Exception("Invalid response from server: " + str);
+            }
+
+            // Step 3: Send AUTH with authentication information to server
+            out.writeUTF("AUTH Ava\n");
+            out.flush();
+            System.out.println("SENT: AUTH Ava");
+
+            // Step 4: Wait for OK response from server
+            str = in.readLine();
+            if (str.startsWith("OK")) {
+                System.out.println("RCVD: " + str);
+            } else {
+                throw new Exception("Invalid response from server: " + str);
+            }
+
+            // Step 5: Send REDY to server
+            out.writeUTF("REDY\n");
+            out.flush();
+            System.out.println("SENT: REDY");
+
+            // Step 6: Wait for JOBN, JOBP, JCPL, RESF, RESR, CHKQ, or NONE message from
+            // server
+            str = in.readLine();
             System.out.println("RCVD: " + str);
 
             in.close();
@@ -30,5 +58,63 @@ public class Client {
         } catch (Exception e) {
             System.out.println(e);
         }
-    }
-}
+    }}
+
+    
+
+    
+    
+
+    
+
+    
+
+    
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+    
+    
+    
+    
+    
+
+    
+
+    
+    
+    
+    
+    
+        
+        
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+    
+        
+    
