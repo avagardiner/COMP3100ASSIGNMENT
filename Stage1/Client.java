0441118d13fd9526456cf2859e3bc4867678527b
avagardiner <88016022+avagardiner@users.noreply.github.com>
2023-04-02 09:57:03 +1000

    Implement identify large server types and lrr algorthm

--- Stage1/Client.java
+++ Stage1/Client.java
@@ -9,6 +9,11 @@ public class Client {
         try {
             Socket s = new Socket("127.0.0.1", 50000);
 
+            int numServers = 10;
+            int[] serverCoreCounts = new int[numServers];
+            int[] serverTypes = new int[numServers];
+            int[] serverIds = new int[numServers];
+
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
@@ -29,9 +34,9 @@ public class Client {
             }
 
             // Step 3: Send AUTH with authentication information to server
-            out.writeUTF("AUTH Ava\n");
+            out.writeUTF("AUTH Ava Gardiner\n");
             out.flush();
-            System.out.println("SENT: AUTH Ava");
+            System.out.println("SENT: AUTH Ava Gardiner");
 
             // Step 4: Wait for OK response from server
             str = in.readLine();
@@ -50,6 +55,47 @@ public class Client {
             // server
             str = in.readLine();
             System.out.println("RCVD: " + str);
+
+            int largestServerType = -1; // initialize to an invalid value
+            int largestServerId = -1;
+            int largestServerCoreCount = -1;
+
+            while (str.startsWith("JOBN")) {
+                // Parse job information
+                String[] jobFields = str.split("\\s+");
+                int jobId = Integer.parseInt(jobFields[2]);
+                int jobSubmitTime = Integer.parseInt(jobFields[3]);
+                int jobEstRuntime = Integer.parseInt(jobFields[4]);
+                int jobCore = Integer.parseInt(jobFields[5]);
+
+                // Find the largest server that can accommodate the job
+                for (int i = 0; i < numServers; i++) {
+                    if (serverCoreCounts[i] >= jobCore) {
+                        if (largestServerType == -1 || serverTypes[i] > largestServerType ||
+                                (serverTypes[i] == largestServerType && serverCoreCounts[i] > largestServerCoreCount)) {
+                            largestServerType = serverTypes[i];
+                            largestServerId = serverIds[i];
+                            largestServerCoreCount = serverCoreCounts[i];
+                        }
+                    }
+                }
+
+                // Send SCHD command to allocate the largest server
+                out.writeUTF("SCHD " + jobId + " " + largestServerType + " " + largestServerId + "\n");
+                out.flush();
+
+                // Wait for OK response from server
+                str = in.readLine();
+                if (str.startsWith("OK")) {
+                    System.out.println("RCVD: " + str);
+                } else {
+                    throw new Exception("Invalid response from server: " + str);
+                }
+
+                // Wait for another job or server status update
+                str = in.readLine();
+                System.out.println("RCVD: " + str);
+            }
 
             // Step 7: Send a message to gracefully terminate the simulation
             out.writeUTF("QUIT\n");
